c5a40de4361ff9d0a96a45d7ae4421694c7185e7
Ozair Couto <zaicoutox@gmail.com>
2025-12-16 23:38:02 -0300

    fix: add banner "use client" in exported code

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-  external: ['react'], // Mark React as external
+  banner: {
+    js: '"use client";',
+  },
+  external: ["react"], // Mark React as external
 });
